bb6514cb2847fdb42936cbe68035032b077fa172
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-03 19:34:08 -0300

    Feat: Cria conexão com o banco de dados

---
+++ src/models/connection.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-expressions */
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const MONGO_DB = process.env.MONGO_DB_URL;
+
+let db = null;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connection = async () => {
+  db
+    ? Promise.resolve(db)
+    : db = await MongoClient.connect(MONGO_DB, options);
+  return db.db('tasks');
+};
+
+module.exports = connection;
