ba9a84853064c92f5c875cc7a59cb95e9e00006e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-03 19:58:47 -0300

    Feat:  Add chamada na api/banco de dados

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const rescue = require('express-rescue');
 
+const Service = require('../services/tasksService');
+
 const tasksRouter = express.Router();
 
-tasksRouter.get('/', rescue(async (req, res) => res.status(200).json({ Nome: 'Fioravante Chiozzi' })));
+tasksRouter.get('/', rescue(async (req, res) => {
+  const tasksAll = await Service.getAll();
+  return res.status(200).json(tasksAll);
+}));
 
 module.exports = tasksRouter;
