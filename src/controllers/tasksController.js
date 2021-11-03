4efd1c0919f6f15470a9dd199012d9edbda7bc30
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-03 20:58:58 -0300

    Feat: Cria rota para criação de tarefas

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const rescue = require('express-rescue');
+const { StatusCodes } = require('http-status-codes');
 
 const Service = require('../services/tasksService');
 
@@ -7,7 +8,19 @@ const tasksRouter = express.Router();
 
 tasksRouter.get('/', rescue(async (req, res) => {
   const tasksAll = await Service.getAll();
-  return res.status(200).json(tasksAll);
+  return res.status(StatusCodes.OK).json(tasksAll);
+}));
+
+tasksRouter.post('/', rescue(async (req, res) => {
+  const date = new Date();
+  const { task, description, status } = req.body;
+  const tasks = await Service.create({
+    task,
+    description,
+    status,
+    date: `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`,
+  });
+  return res.status(StatusCodes.CREATED).json({ tasks });
 }));
 
 module.exports = tasksRouter;
