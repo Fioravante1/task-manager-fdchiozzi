a0522861a78d073c5974c0d235bcf3763f6c70c4
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-03 20:59:29 -0300

    Feat: Cria query para criação de tarefa no banco de dados

--- src/models/tasksModel.js
+++ src/models/tasksModel.js
@@ -5,6 +5,23 @@ const getAll = async () => {
   return db.collection('Tasks').find().toArray();
 };
 
+const create = async (tasks) => {
+  const {
+    task, description, status, date,
+  } = tasks;
+  const db = await connection();
+  const newCard = await db.collection('Tasks').insertOne(tasks);
+  const { insertedId } = newCard;
+  return {
+    task,
+    description,
+    status,
+    date,
+    insertedId,
+  };
+};
+
 module.exports = {
   getAll,
+  create,
 };
