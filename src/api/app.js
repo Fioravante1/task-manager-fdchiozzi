940f6d14a8b22f9859929f545a885184661327cd
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-03 21:23:44 -0300

    Feat: Config cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -6,6 +7,7 @@ const bodyParser = require('body-parser');
 const tasksController = require('../controllers/tasksController');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/tasks', tasksController);
 
