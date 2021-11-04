efd4e772345545397d1db4d71cf458a1e267924e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-03 21:12:11 -0300

    Feat: Service que busca a tarefa por id

--- src/services/tasksService.js
+++ src/services/tasksService.js
@@ -1,5 +1,13 @@
 const Model = require('../models/tasksModel');
 
+const findById = async (id) => {
+  const card = await Model.findById(id);
+  if (!card) {
+    return null;
+  }
+  return card;
+};
+
 const getAll = async () => {
   const tasks = await Model.getAll();
   return tasks;
