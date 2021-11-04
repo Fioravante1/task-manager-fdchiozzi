ddb533b4996061b305a870df3a0f406453c2673e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-03 21:14:24 -0300

    Feat: Controller para busca por id e para update da tarefa

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -11,6 +11,15 @@ tasksRouter.get('/', rescue(async (req, res) => {
   return res.status(StatusCodes.OK).json(tasksAll);
 }));
 
+tasksRouter.get('/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+  const card = await Service.findById(id);
+  if (!card) {
+    return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Tarefa não encontrada' });
+  }
+  return res.status(StatusCodes.OK).json(card);
+}));
+
 tasksRouter.post('/', rescue(async (req, res) => {
   const date = new Date();
   const { task, description, status } = req.body;
@@ -21,6 +30,12 @@ tasksRouter.post('/', rescue(async (req, res) => {
     date: `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`,
   });
   return res.status(StatusCodes.CREATED).json({ tasks });
+}));
+
+tasksRouter.put('/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+  const card = await Service.update(id, req.body);
+  return res.status(StatusCodes.OK).json(card);
 }));
 
 module.exports = tasksRouter;
